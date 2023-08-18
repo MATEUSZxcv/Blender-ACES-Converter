810e17ccbd771be5b8530a8028ca7a425dbbdfe8
MATEUSZxcv <56976165+MATEUSZxcv@users.noreply.github.com>
2023-08-18 16:16:35 +0200

    error handling

--- Blender-ACES-Converter.py
+++ Blender-ACES-Converter.py
@@ -22,38 +22,58 @@ def SaveColorSpaces(self, context):
     directory = os.path.dirname(filepath)
     tmp_path = os.path.join( directory , "ACES-Converter_tmp.txt")
 
-    f=open(tmp_path,'w')
-        
+    if os.path.isfile(tmp_path):
+        self.report({'WARNING'}, f"File with colorspaces already exists and was overwritten.")
+
+    try:
+        f=open(tmp_path,'w')
+    except PermissionError:
+        self.report({'ERROR'}, f"No permission - Can't save file with colorspaces! (Is blendfile saved in accessible location?)")
+        return
+
     for m in bpy.data.images:
         f.write(m.colorspace_settings.name+"\n")
     f.close()
+
+    self.report({'INFO'}, f"Colorspaces saved to %s" %(tmp_path))
         
 def LoadColorSpaces(self, context):
     filepath = bpy.data.filepath
     directory = os.path.dirname(filepath)
     tmp_path = os.path.join( directory , "ACES-Converter_tmp.txt")
+
+    try:
+        f=open(tmp_path, 'r')
+    except FileNotFoundError:
+        self.report({'ERROR'}, f"File with colorspaces doesn't exist!")
+        return
     
-    f=open(tmp_path, 'r')
     color = f.read().splitlines()
         
     i = 0
-    for m in bpy.data.images:
-        if i == len(color):
-            break
-        if color[i] == "sRGB":
-            m.colorspace_settings.name = 'role_matte_paint'
-        elif color[i] == "Non-Color":
-            m.colorspace_settings.name = 'role_data'
-        elif color[i] == "Linear":
-            m.colorspace_settings.name = 'Utility - Linear - sRGB'
-        i = i+1
-        
+    try:
+        for m in bpy.data.images:
+            if i == len(color):
+                break
+            if color[i] == "sRGB":
+                m.colorspace_settings.name = 'role_matte_paint'
+            elif color[i] == "Non-Color":
+                m.colorspace_settings.name = 'role_data'
+            elif color[i] == "Linear":
+                m.colorspace_settings.name = 'Utility - Linear - sRGB'
+            i = i+1
+    except TypeError:
+        self.report({'ERROR'}, f"Can't find colorspaces - wrong ACES config!")
+        f.close()
+        return
+
     f.close()
     os.remove(tmp_path)
+    self.report({'INFO'}, f"Colorspaces loaded in ACES")
 
-class Save_operator(Operator):
+class ACESCONV_OT_save(Operator):
     """ Save colorspaces """
-    bl_idname = "save.operator"
+    bl_idname = "object.save"
     bl_label = "Save"
     bl_options = {"REGISTER", "UNDO"}
 
@@ -63,12 +83,11 @@ class Save_operator(Operator):
 
     def execute(self, context):
         SaveColorSpaces(self, context)
-        self.report({'INFO'}, f"Colorspaces saved")
         return {'FINISHED'}
     
-class Load_operator(Operator):
+class ACESCONV_OT_load(Operator):
     """ Load colorspaces in ACES """
-    bl_idname = "load.operator"
+    bl_idname = "object.load"
     bl_label = "Load"
     bl_options = {"REGISTER", "UNDO"}
 
@@ -78,11 +97,10 @@ class Load_operator(Operator):
 
     def execute(self, context):
         LoadColorSpaces(self, context)
-        self.report({'INFO'}, f"Colorspaces loaded in ACES")
         return {'FINISHED'}
 
 
-class sidebar(Panel):
+class ACESCONV_PT_siebar(Panel):
     bl_label = "ACES Converter"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
@@ -90,14 +108,14 @@ class sidebar(Panel):
 
     def draw(self, context):
         col = self.layout.column(align=True)
-        prop = col.operator(Save_operator.bl_idname, text="Save")
-        prop2 = col.operator(Load_operator.bl_idname, text="Load")
+        col.operator(ACESCONV_OT_save.bl_idname, text="Save", icon="IMPORT")
+        col.operator(ACESCONV_OT_load.bl_idname, text="Load", icon="EXPORT")
+        #TODO: save-load other way
 
- 
 classes = [
-    Save_operator,
-    Load_operator,
-    sidebar,
+    ACESCONV_OT_save,
+    ACESCONV_OT_load,
+    ACESCONV_PT_siebar,
 ]
 
 def register():
